f9778e4f408907e14ae9587a199a98a5c14045f0
XanderUZZZER <xander_uzzzer@mail.ru>
2017-05-09 01:53:50 +0300

    0905170153

--- OOPGameSnakeV2/Program.cs
+++ OOPGameSnakeV2/Program.cs
@@ -11,16 +11,14 @@ namespace OOPGameSnakeV2
         {
             if (Console.LargestWindowHeight == 72) // Full HD == 72  HD Ready == 44
             {
-                Console.WindowWidth = 70;     //Window width in columns
                 Console.WindowHeight = 48;    //Window height in rows
-                Console.SetBufferSize(Console.WindowWidth, Console.WindowHeight);
             }
             else
             {
-                Console.WindowWidth = 70;     //Window width in columns
                 Console.WindowHeight = 44;    //Window height in rows
-                Console.SetBufferSize(Console.WindowWidth, Console.WindowHeight);
             }
+            Console.WindowWidth = 70;     //Window width in columns
+            Console.SetBufferSize(Console.WindowWidth, Console.WindowHeight);
             Console.Title = "Snake The Game";
             Console.BackgroundColor = ConsoleColor.Gray;
             Console.CursorVisible = false;
