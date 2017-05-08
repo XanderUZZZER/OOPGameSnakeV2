0f7af0c8c48c074431a51850c73b0bbc8441a590
XanderUZZZER <xander_uzzzer@mail.ru>
2017-05-09 01:52:38 +0300

    Resolution implemented for both (Fhd, hdr)

--- OOPGameSnakeV2/Program.cs
+++ OOPGameSnakeV2/Program.cs
@@ -1,5 +1,7 @@
 ﻿using NConsoleGraphics;
 using System;
+using System.Windows;
+using System.Runtime.InteropServices;
 
 namespace OOPGameSnakeV2
 {
@@ -7,38 +9,26 @@ namespace OOPGameSnakeV2
     {
         static void Main(string[] args)
         {
-            Console.WindowWidth = 70;     //Window width in columns
-            Console.WindowHeight = 44;    //Window height in rows
-            Console.SetBufferSize(Console.WindowWidth, Console.WindowHeight);
+            if (Console.LargestWindowHeight == 72) // Full HD == 72  HD Ready == 44
+            {
+                Console.WindowWidth = 70;     //Window width in columns
+                Console.WindowHeight = 48;    //Window height in rows
+                Console.SetBufferSize(Console.WindowWidth, Console.WindowHeight);
+            }
+            else
+            {
+                Console.WindowWidth = 70;     //Window width in columns
+                Console.WindowHeight = 44;    //Window height in rows
+                Console.SetBufferSize(Console.WindowWidth, Console.WindowHeight);
+            }
             Console.Title = "Snake The Game";
-            //Console.BackgroundColor = ConsoleColor.Gray;
-            //Console.CursorVisible = false;
-            //Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.CursorVisible = false;
+            Console.Clear();
 
             ConsoleGraphics graphics = new ConsoleGraphics();
-            Console.WriteLine($"Console.WindowWidth {Console.WindowWidth}");
-            Console.WriteLine($"graphics.ClientWidth {graphics.ClientWidth}");
-            Console.WriteLine($"Console.WindowHeight {Console.WindowHeight}");
-            Console.WriteLine($"graphics.ClientHeight {graphics.ClientHeight}");
-            Console.WriteLine($"relative Height {graphics.ClientHeight / Console.WindowHeight}");
-            Console.WriteLine($"relative Width {graphics.ClientWidth / Console.WindowWidth}");
-            Console.WindowWidth = 69;     //Window width in columns
-            Console.WindowHeight = 43;    //Window height in rows
-            Console.WriteLine($"Console.WindowWidth {Console.WindowWidth}");
-            Console.WriteLine($"graphics.ClientWidth {graphics.ClientWidth}");
-            Console.WriteLine($"Console.WindowHeight {Console.WindowHeight}");
-            Console.WriteLine($"graphics.ClientHeight {graphics.ClientHeight}");
-            //Console.WindowWidth 70
-            //graphics.ClientWidth 507
-            //Console.WindowHeight 48
-            //graphics.ClientHeight 689
-            //Console.WindowWidth 69
-            //graphics.ClientWidth 483
-            //Console.WindowHeight 47
-            //graphics.ClientHeight 658
-            //GameEngine engine = new GameEngineSnake(graphics);
-            //engine.Start();
-            Console.ReadLine();
+            GameEngine engine = new GameEngineSnake(graphics);
+            engine.Start();
         }
     }
 }
